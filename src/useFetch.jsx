8e9a9ed753911dff147c6b254301d840d18571aa
nsr <githubnsr@gmail.com>
2024-04-08 22:45:12 +0530

    removed throttle

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -1,41 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect,useState } from "react";
 
-const useFetch = (url, maxRetries = 3, initialDelay = 1000) => {
-    const [data, setData] = useState(null);
-    const [pending, setPending] = useState(false);
-    const [err, setErr] = useState('');
-    const [retryCount, setRetryCount] = useState(0);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                setPending(true);
-                const response = await fetch(url);
-                if (!response.ok) {
-                    throw new Error('The data could not be fetched');
+const useFetch = (url) => {
+    const [data, setData]=useState(null);
+    const [pending,setPending]=useState(false);
+    const [err,setErr]=useState('');
+    
+        useEffect(()=>{
+            const fetchData = async () =>{
+            try{
+                setPending(!pending)
+                const response = await fetch(url)
+                if (!response.ok){
+                    throw new Error('The data could not be fetched')
                 }
-                const responseData = await response.json();
-                setData(responseData);
-            } catch (e) {
-                if (retryCount < maxRetries) {
-                    
-                    const delay = Math.pow(2, retryCount) * initialDelay;
-                    await new Promise(resolve => setTimeout(resolve, delay));
-                    setRetryCount(retryCount + 1);
-                    fetchData(); 
-                } else {
-                    setErr(e.message);
-                    console.log(e.message);
-                }
-            } finally {
-                setPending(false); 
+                const responseData=await response.json()
+                setPending(!pending) 
+                setData(responseData)
+            }catch (e){
+                setErr(e.message)
+                setPending(!pending)
+                console.log(e.message)
             }
-        };
+        }
+        setTimeout(()=>{
 
-        fetchData(); 
-    }, [url, maxRetries, initialDelay]); 
-
-    return { data, pending, err };
-};
-
+            fetchData();
+        },10000)
+        },[])
+    
+    
+       
+    
+    return {data,pending,err};
+}
+ 
 export default useFetch;
